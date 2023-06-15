63961be420b2132568dd888be8cda4f356307ee5
jangel <jorgeangel.mn@gmail.com>
2023-06-14 20:32:01 -0600

    Updates

---
+++ precache-manifest.bcb2bbc1cab6a39c189de81aa5f12d74.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ef41ac929926bf272a628a7035c8a9b",
+    "url": "/about/index.html"
+  },
+  {
+    "revision": "40df7ea12372ec1b5418",
+    "url": "/about/static/css/main.f77d7a38.chunk.css"
+  },
+  {
+    "revision": "f0585afd58bab4c12498",
+    "url": "/about/static/js/2.9a2180c5.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/about/static/js/2.9a2180c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40df7ea12372ec1b5418",
+    "url": "/about/static/js/main.24f5724e.chunk.js"
+  },
+  {
+    "revision": "1d51d628a906648936ec",
+    "url": "/about/static/js/runtime-main.61611382.js"
+  },
+  {
+    "revision": "b88924d5d914bf9f3885f87c2ed76477",
+    "url": "/about/static/media/sad-icon.b88924d5.png"
+  },
+  {
+    "revision": "b9d7d448a09d6aacb45d21aab2c6fd53",
+    "url": "/about/static/media/your photo.b9d7d448.png"
+  }
+]);
